077e4bc121b3e6d72bf29dfc2b85f0f2c7b12c72
felipewnp <34146218+felipewnp@users.noreply.github.com>
2024-04-05 16:44:44 -0300

    Adding make swap file script.

--- cloudshell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -sOL https://github.com/getsops/sops/releases/download/v3.8.1/sops-v3.8.1.linux.amd64
-
-mv sops-v3.8.1.linux.amd64 /usr/local/bin/sops
-
-chmod +x /usr/local/bin/sops
---
+++ make-swap-file.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -eo pipefail
+
+# VARIABLES
+SWAP_PATH=/swapfile
+SWAP_SIZE=2G
+
+printf "\nCreating ${SWAP_PATH} file with sixe of ${SWAP_SIZE}\n\n"
+fallocate -l ${SWAP_SIZE} ${SWAP_PATH}
+
+printf "\n\n\nSetting 0600 permission to ${SWAP_PATH}\n\n"
+chmod 600 ${SWAP_PATH}
+
+printf "\n\n\nFormating ${SWAP_PATH} as swap.\n\n"
+mkswap ${SWAP_PATH}
+
+printf "\n\n\nMaking a backup of fstab.\n\n"
+cp /etc/fstab /etc/fstab.bak
+
+printf "\n\n\nEnabling ${SWAP_PATH} as swap.\n\n"
+swapon ${SWAP_PATH}
+echo "${SWAP_PATH} none swap sw 0 0" >> /etc/fstab
+
+printf "\n\n\nSetting vm.swappiness=10.\n\n"
+sysctl vm.swappiness=10
+echo "vm.swappiness=10" >> /etc/sysctl.conf
+
+printf "\n\n\nSetting vm.vfs_cache_pressure=50.\n\n"
+sysctl vm.vfs_cache_pressure=50
+echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.conf
+
+printf "\n\n\nDone! Swap status:\n\n"
+free -h
+printf "\n\n\n"
